67058dab8b5c3ff8c4fbd5a4bd4975678cc21f00
brianrddaw <brianramirezdaw@gmail.com>
2024-03-05 17:58:10 +0100

    Añadido header - user-views -> user

--- resources/views/user-views/components/header.blade.php
+++ resources/views/user-views/components/header.blade.php
@@ -1,23 +1,11 @@
-<header>
-    <img src="{{ asset('imgs/logo.png') }}" alt="">
-    <h1>hola</h1>
+<header class="grid grid-cols-4 justify-items-center items-center  bg-stone-800 p-2 ">
+    <img src="{{ asset('imgs/logo.png') }}" alt="" class="w-16 ">
+    <h1 class="text-white col-span-2">{{ $title ?? 'nombre_resturante'}}</h1>
+    <nav class="text-white">
+        <p>nav</p>
+    </nav>
 </header>
 
 <style>
-    header {
-        width: 100%;
-        height: 100px;
-        background-color: #f1f1f1;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
 
-    img {
-        width: 200px;
-    }
-
-    h1 {
-        color: blue
-    }
 </style>
