48fe0ac736954c3efe5f5e334e9947de52aee8b8
brianramirezdaw <brianramirezdaw@gmail.com>
2024-03-05 11:48:20 +0000

    Primer commit

---
+++ resources/views/layouts/html-layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    @yield('content')
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            min-width: 0;
+        }
+    </style>
+</body>
+</html>
---
+++ resources/views/user-views/main.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.html-layout')
+
+@section('title', 'Main')
+@section('content')
+
+    <h1>Hola</h1>
+
+    <style>
+
+    </style>
+@endsection
+
+
+
+
