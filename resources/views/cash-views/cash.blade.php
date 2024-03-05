47ad4a215324a66245fc6de727da3972348758d4
brianramirezdaw <brianramirezdaw@gmail.com>
2024-03-05 12:15:19 +0000

    Añadida carpetas varias en views

---
+++ resources/views/cash-views/cash.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.html-layout')
+
+@section('title', 'Cash')
+@section('content')
+
+    <h1>Cash</h1>
+
+    <style>
+
+    </style>
+@endsection
---
+++ resources/views/user-views/user.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.html-layout')
+
+@section('title', 'User')
+@section('content')
+
+    <h1>User</h1>
+
+    <style>
+
+    </style>
+@endsection
+
+
+
+
