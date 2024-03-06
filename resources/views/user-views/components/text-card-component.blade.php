aa4246b8392c5dcef90bfd4bb373701ab769da11
brianrddaw <brianramirezdaw@gmail.com>
2024-03-06 02:19:42 +0100

    Sidebar añadida, mejorado estilo user view, agregados varios componentes

---
+++ resources/views/user-views/components/text-card-component.blade.php
@@ -0,0 +1,3 @@
+<div class="flex flex-col justify-center items-center p-5 bg-red-500 rounded w-fit min-w-52 text-white hover:bg-red-400 hover:cursor-pointer">
+    <p class="text-lg font-sans">{{ $message }}</p>
+</div>
