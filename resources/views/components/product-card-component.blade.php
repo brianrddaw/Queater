8c17590c5f278fda2033ec742aa09da633932da5
brianramirezdaw <brianramirezdaw@gmail.com>
2024-03-06 10:19:27 +0000

    Product card casi terminada

--- resources/views/components/product-card-component.blade.php
+++ resources/views/components/product-card-component.blade.php
@@ -1,20 +1,27 @@
-<div class=" border-2 border-red-700 grid grid-cols-2 grid-rows-2 w-fit  h-24 w-60 rounded-md">
-    <div class=" border-2 border-indigo-700 row-span-2 h-fit w-fit">
-        <img class="w-20 border-2 border-red-700 " src="../imgs/burguer.webp" alt="product_image">
-        <span class="flex gap-1 border border-green-700">
+<div class="  grid grid-cols-2  -fit  h-fit w-60 rounded-md">
+    <div class="row-span-2 h-fit w-fit">
+        <img class="w-20  " src="../imgs/burguer.webp" alt="product_image">
+        <span class="flex gap-1 ">
             <div class="w-4 h-4 bg-orange-500 rounded-full"></div>
             <div class="w-4 h-4 bg-orange-500 rounded-full"></div>
             <div class="w-4 h-4 bg-orange-500 rounded-full"></div>
             <div class="w-4 h-4 bg-orange-500 rounded-full"></div>
         </span>
     </div>
-    <div class=" border-2 border-blue-700 h-fit">
-        <h2>Cangreburguer</h2>
-        <p class="text-sm max-w-12 max-h-10 text-ellipsis overflow-clip">Jugosa carne de res a la parrilla con lechuga, tomate, cebolla y pepinillos, servida en un suave pan de hamburguesa. Acompañada de papas fritas crujientes.</p>
+    <div class="  h-fit">
+        <h2 class="font-bold">Cangreburguer</h2>
+        <p class="text-xs  w-full max-h-10 text-ellipsis overflow-hidden">Jugosa carne de res a la parrilla con lechuga, tomate, cebolla y pepinillos, servida en un suave pan de hamburguesa. Acompañada de papas fritas crujientes.</p>
     </div>
-    <div class="flex h-fit border-2 border-green-700">
-        <p>precio</p>
-        <div>añadir</div>
+    <div class="flex items-end justify-between h-fit ">
+        <p class="text-xs ">19.99</p>
+        <div class="flex items-center text-xs">
+            Añadir
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+        </div>
+
+
     </div>
 </div>
 
