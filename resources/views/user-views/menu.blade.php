3903324eb1ce98ef92837d973ebf9f3764995429
brianrddaw <brianramirezdaw@gmail.com>
2024-03-09 14:58:58 +0100

    reestructuración entera del proyecto | lógica del carrito hecha y optimizada

--- resources/views/user-views/menu.blade.php
+++ resources/views/user-views/menu.blade.php
@@ -20,13 +20,6 @@
             </ul>
         @endforeach
 
-        {{-- @foreach ( $products as $product )
-            @if ($product->availability)
-
-                @include('user-views.user-product.product-card', ['product' => $product])
-            @endif
-        @endforeach --}}
-
 
         @include('user-views.user-cart.user-cart', ['orders' => []])
 
@@ -39,13 +32,17 @@
 
     <script>
         var order = {};
+        var orderTotal = 0;
+        var cartProductsQuantity = 0;
+
 
         window.onload = function()
         {
+            // validar si hay algo en el carrito para mostrarlo
             if (Object.keys(order).length == 0) {
-                console.log("No hay productos en el pedido");
-                $('#cart').hide();
-            };
+                $('#cart').css('transform', 'translateY(10%)');
+            }
+
         }
 
         function addToOrder(id, price, name)
@@ -54,6 +51,11 @@
             if (order[id])
             {
                 order[id].quantity++;
+
+                // Actualizar la tarjeta
+                let productCard = $('#cartProductCard' + id);
+                productCard.find('.product-quantity').text( order[id].quantity );
+
             }
             else
             {
@@ -61,50 +63,214 @@
                     id: id,
                     name: name,
                     price: price,
-                    quantity: 1
+                    quantity: 1,
+
                 };
+
+                addToOrderHtml(order[id]);
             }
-            if(Object.keys(order).length > 0){
-                $('#cart').show();
+
+            cartProductsQuantity ++;
+            updateCartQuantityText(cartProductsQuantity);
+
+            orderTotal += price;
+            $('.order-total').text(orderTotal.toFixed(2) + ' €');
+
+            if(Object.keys(order).length = 1){
+                $('#cart').css('transform', 'translateY(0%)');
             }
-            addToOrderHtml(order[id]);
 
 
-            console.log("Este es el producto",id, price,name);
 
         }
 
 
         function addToOrderHtml(product) {
-            console.log("Este es el producto para html", product);
 
+            // Obtener el contenedor del carrito
             var cartCtn = $('#cart-products-container');
 
+            // Crear la tarjeta
+            var productCard = $('<div id="cartProductCard' + product.id + '"class="cart-product-card max-w-80 grid grid-cols-9 items-center rounded  min-h-14 h-14 bg-orange-50 text-orange-950 text-xs"></div>');
+
+            // IMG
+            var productImg = $('<img class="col-span-2 w-14 h-14 object-fit border-2 border-orange-950 bg-orange-950 rounded-l object-cover" src="{{ asset('imgs/burguer.webp') }}" alt="">');
+
+            // INFO CONTAINER
+            var productInfo = $('<div class="flex flex-col justify-between pl-1 py-1 text-base m w-full h-full col-span-5 "></div>');
 
 
-            var productCard = $('<div id=cartProductCard'+ product.id +' class="flex items-center rounded-r-lg rounded-l-full min-h-12 h-12 pr-4 bg-orange-50 text-orange-950"></div>');
-            var productImg = $('<img class="w-12 h-12 mr-4 object-fit border-2 border-orange-500 bg-orange-950 rounded-full object-fit" src="{{ asset("imgs/burguer.webp") }}" alt="">');
-            var productInfo = $('<div class="flex flex-col justify-between text-base max-w-24 w-full h-full pt-2 pb-1"></div>');
+            // PRODUCT NAME
             var productName = $('<p class="font-bold leading-none max-h-4 overflow-hidden whitespace-nowrap text-ellipsis">' + product.name + '</p>');
-            var productPrice = $('<p class="leading-none max-h-4 overflow-hidden whitespace-nowrap text-ellipsis">' + product.price + ' €</p>');
-            var productButtons = $('<div class="flex justify-between items-center ml-auto gap-2"></div>');
-            var minusButton = $('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" /></svg>');
-            var quantity = $('<p class="flex items-center justify-center v leading-none w-4 text-xl text-center tabular-nums">' + product.quantity + '</p>');
-            var plusButton = $('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" /></svg>');
-
             productInfo.append(productName);
-            productInfo.append(productPrice);
 
-            productButtons.append(minusButton);
-            productButtons.append(quantity);
-            productButtons.append(plusButton);
 
+            // QUANTITY
+
+            // QUANTITY CONTAINER
+            var quantity = $('<div class="flex justify-between items-center max-w-20"></div>');
+
+            quantity.append('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 substract-button"><path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" /></svg>');
+            quantity.append('<p class="product-quantity flex items-center justify-center v leading-none w-4 text-lg text-center tabular-nums">' + product.quantity + '</p>');
+            quantity.append('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 add-button"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" /></svg>');
+            productInfo.append(quantity);
+
+
+            // DELETE ICON & PRICE
+            var productButtons = $('<div class="flex flex-col justify-between py-1 pr-1 w-full max-full h-full col-span-2"></div>');
+
+
+            // DELETE ICON
+            var deleteIcon = $('<div class="delete-product-button cursor-pointer flex items-center gap-2 ml-auto"></div>');
+            deleteIcon.append('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" /></svg>');
+            productButtons.append(deleteIcon);
+
+            // PRICE
+            var productPrice = $('<p class="leading-none max-h-4 overflow-hidden whitespace-nowrap text-ellipsis text-sm ml-auto ">' + product.price + ' €</p>');
+            productButtons.append(productPrice);
+
+
+            // Agregar los elementos a la tarjeta
             productCard.append(productImg);
             productCard.append(productInfo);
             productCard.append(productButtons);
 
+            // Agregar la tarjeta al contenedor del carrito
             cartCtn.append(productCard);
+
+            // Agregar la tarjeta al contenedor del carrito
+            cartCtn.append(productCard.hide().fadeIn(150));
+
+
+            // Desasociar eventos de los elementos existentes
+            $('.delete-product-button').off('click');
+            $('.substract-button').off('click');
+            $('.add-button').off('click');
+
+            // Volver a asociar los eventos a los elementos
+            $('.delete-product-button').on('click', function() {
+                removeFromOrder($(this).closest('.cart-product-card'))
+            });
+
+            $('.substract-button').on('click', function() {
+                modifyQuantity($(this), -1);
+            });
+
+            $('.add-button').on('click', function() {
+                modifyQuantity($(this), 1);
+            });
+
+
+
         }
+
+
+        function updateCartQuantityText(quantity){
+            $('.cart-quantity').text(quantity);
+        }
+
+        // Función genérica para modificar la cantidad
+        function modifyQuantity($element, amount) {
+            const productCard = $element.closest('.cart-product-card');
+            const productQuantity = productCard.find('.product-quantity');
+            const idProduct = productCard.attr('id').replace('cartProductCard', '');
+
+            const newQuantity = parseInt(productQuantity.text()) + amount;
+            if (newQuantity < 1) return;
+
+            // Actualizar el carrito
+            cartProductsQuantity += amount;
+            productQuantity.text(newQuantity);
+            updateCartQuantityText(cartProductsQuantity);
+
+            // Actualizar el total
+            orderTotal += amount * order[idProduct].price;
+            orderTotal = Math.max(0, orderTotal);
+            $('.order-total').text(orderTotal.toFixed(2) + '€');
+
+            // Actualizar el array de orden
+            order[idProduct].quantity += amount;
+        }
+
+
+    // Función para eliminar un producto del pedido
+        function removeFromOrder($productCard) {
+            const idProduct = $productCard.attr('id').replace('cartProductCard', '');
+
+             if (order[idProduct]) {
+                // Actualizar el carrito
+                const quantityToRemove = order[idProduct].quantity;
+                cartProductsQuantity -= quantityToRemove;
+                updateCartQuantityText(cartProductsQuantity);
+
+                // Actualizar el total
+                orderTotal -= order[idProduct].price * quantityToRemove;
+                orderTotal = Math.max(0, orderTotal);
+                $('.order-total').text(orderTotal.toFixed(2) + '€');
+
+                // Actualizar el array de orden
+                delete order[idProduct];
+
+                // Eliminar el elemento HTML del carrito
+                $productCard.fadeOut(150, function() {
+                    $(this).remove();
+                });
+            }
+        }
+        // function addQuantity($element){
+
+        //     //  obtenemos la id tarjeta
+        //     let productCard = $element.parent().parent().parent();
+        //     let productQuantity = productCard.find('.product-quantity');
+
+        //     idProduct = productCard.attr('id').replace('cartProductCard', '');
+
+        //    // actualizar el carrito
+        //     cartProductsQuantity ++;
+        //     productQuantity.text(cartProductsQuantity);
+        //     updateCartQuantityText(cartProductsQuantity);
+
+        //     // actualizar el total
+        //     orderTotal += order[idProduct].price;
+        //     orderTotal = Math.max(0, orderTotal); // Asegurarse de que el total nunca sea menor que cero
+        //     $('.order-total').text(orderTotal.toFixed(2) + '€');
+
+
+        //     order[idProduct].quantity++;
+
+        // }
+
+        // function substractQuantity($element){
+
+
+        //     //  obtenemos la id tarjeta
+        //     let productCard = $element.parent().parent().parent();
+        //     let productQuantity = productCard.find('.product-quantity');
+
+        //     idProduct = productCard.attr('id').replace('cartProductCard', '');
+
+        //     if (productQuantity.text() == 1) {
+        //         return;
+        //     }
+
+        //    // actualizar el carrito
+        //     cartProductsQuantity --;
+        //     productQuantity.text(cartProductsQuantity);
+        //     updateCartQuantityText(cartProductsQuantity);
+
+        //     // actualizar el total
+        //     orderTotal -= order[idProduct].price;
+        //     orderTotal = Math.max(0, orderTotal); // Asegurarse de que el total nunca sea menor que cero
+        //     $('.order-total').text(orderTotal.toFixed(2) + '€');
+
+
+        //     order[idProduct].quantity--;
+
+
+
+        // }
+
+
 
 
         function makeOrder()
@@ -134,6 +300,8 @@
             });
         }
     </script>
+
+
     <style>
 
         .eat-here-main{
--- resources/views/user-views/user-cart/user-cart.blade.php
+++ resources/views/user-views/user-cart/user-cart.blade.php
@@ -1,36 +1,46 @@
-<div id="cart" class=" user-cart fixed bottom-0 top-[94vh] left-[10vw]  w-[80vw] h-[90vh] bg-orange-500 z-10 grid grid-rows-12 p-4 pt-0 gap-4 rounded-t-lg text-orange-100" style="transition: all 0.3s ease;">
+<div id="cart" class="user-cart fixed bottom-0 top-[92vh] left-[10vw]  w-[80vw] h-[90vh] bg-orange-500 z-10 grid grid-rows-10 p-4 pt-0 gap-4 rounded-t-lg text-orange-100" style="transition: all 0.3s ease;">
 
     {{-- ENCABEZADO CARRITO --}}
-    <div class="grid grid-cols-3 justify-items-center items-center  w-full  h-[6vh] ">
+    <div class="grid grid-cols-3 justify-items-center items-center  w-full  h-[8vh] pt-1 ">
 
         {{-- PRECIO TOTAL --}}
-        <p class="font-bold text-md">0 €</p>
+        <p class="order-total font-bold text-md">0 €</p>
 
         {{-- SVGS --}}
         <svg id="cart-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-6 h-6 animate-bounce mt-2">
             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
         </svg>
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>
+        <div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 absolute">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+
+            </svg>
+            <span class="cart-quantity flex items-center justify-center font-bold text-orange-950 w-5 h-5 bg-orange-50 rounded-full  relative -top-2 left-4 text-xs leading-none text-center whitespace-nowrap">
+                0
+            </span>
+
+        </div>
         {{-- END SVGS --}}
 
     </div>
 
+    <div class="w-full h-0.5 bg-orange-50 rounded-full my-auto"></div>
+
     {{-- TARJETAS DE PRODUCTOS DEL CARRITO --}}
-    <div id="cart-products-container" class=" flex flex-col gap-3 row-span-11 h-full overflow-scroll pr-4">
+    <div id="cart-products-container" class=" flex flex-col w-full gap-3 row-span-9 h-full overflow-scroll">
 
-
+        {{-- @livewire('cart-product-card') --}}
 
     </div>
 
     {{-- BOTON FINALIZAR PEDIDO --}}
-    {{-- <button onclick="makeOrder()" class=" bottom-4 w-full bg-orange-50 py-4 rounded-lg text-orange-950 font-bold">
+    <button onclick="makeOrder()" class="flex justify-center items-center bottom-4 w-full bg-orange-50 py-4 rounded-lg text-orange-950 font-bold row-span-1">
 
         Terminar pedido
 
-    </button> --}}
+    </button>
 
 </div>
 
@@ -46,9 +56,9 @@
         arrow.addEventListener('click', function(){
 
             if (!cartIsOpen) {
-                cart.style.transform = 'translateY(-92%)';
+                cart.style.transform = 'translateY(-90%)';
             } else {
-                cart.style.transform = 'translateY(-0%)';
+                cart.style.transform = 'translateY(0%)';
             }
             arrow.classList.toggle('rotate-180');
             arrow.classList.toggle('animate-bounce');
