207e819937700372f19b82f41ef1460c110aeb23
brianramirezdaw <brianramirezdaw@gmail.com>
2024-03-12 13:05:54 +0000

    Añadido canvas | hechas tarjetas pedido cash

--- resources/views/cash-views/cash.blade.php
+++ resources/views/cash-views/cash.blade.php
@@ -6,35 +6,62 @@
     <main class="w-full h-full min-h-[calc(100vh-3.5rem)] p-4  flex flex-col gap-6">
         <section>
 
-            <canvas class="border border-orange-950 w-full h-[50vh]"></canvas>
+            <div id="canvas" class="dragging-container w-full h-[50vh] bg-gray-50 rounded-lg drop-shadow-md">
+                <div class="draggable w-[50px] h-[50px] ">
+                    <img src="../imgs/table.webp" alt="">
+                </div>
+            </div>
+
 
         </section>
-        <section class="w-full h-fit flex flex-wrap flex-col gap-4  overflow-scroll">
+        <section class="w-full h-fit flex flex-wrap flex-col gap-4">
 
             <div class="flex  gap-2" >
                 <button class="bg-orange-500 w-full text-orange-50 font-bold text-2xl rounded-md">Mesa</button>
                 <button class="bg-orange-500 w-full text-orange-50 font-bold text-2xl rounded-md">Para llevar</button>
             </div>
 
+            {{-- Orders container --}}
             <div class="flex flex-col gap-4">
 
-                <div class="w-full h-20 border  rounded-lg bg-gray-100 drop-shadow-lg flex flex-col items-center justify-center gap-2">
+                {{-- orders --}}
+                <div class="flex flex-col gap-4 rounded-lg bg-gray-100 drop-shadow-lg">
+                    <div class=" w-full h-20  grid grid-cols-4 items-center gap-2">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                    </svg>
-                    <h2 class="text-2xl">Mesa</h2>
+                        <span class="flex flex-col justify-center items-left text-lg ml-4">
+                            <p>Nº Pedido</p>
+                            <p class="text-3xl font-bold">37</p>
 
+                        </span>
+                        <span class="flex flex-col justify-center items-center text-lg">
+                            <p>Hora</p>
+                            <p class="text-3xl font-bold">14:37</p>
+
+                        </span>
+                        <span class="flex flex-col justify-center items-center text-lg">
+                            <p>Mesa</p>
+                            <p class="text-3xl font-bold">37</p>
+
+                        </span>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="arrow-icon w-10 h-10 ml-auto mr-4 transition-all">
+                            <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clip-rule="evenodd" />
+                        </svg>
+
+
+
+                    </div>
+
+                    {{-- Comments section --}}
+                    <section class="comments-section hidden flex-col gap-2 w-full p-4">
+
+                        <h2 class="text-xl uppercase font-bold">Comentarios</h2>
+
+                        <textarea name="client-comments" id="client-comments" cols="30" rows="5" class="w-full rounded-md p-2"></textarea>
+
+                    </section>
 
                 </div>
-                <div class="w-full h-20 border  rounded-lg bg-gray-100 drop-shadow-lg flex flex-col items-center justify-center gap-2">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                    </svg>
-                    <h2 class="text-2xl">Para llevar</h2>
-                </div>
-
 
             </div>
 
@@ -44,4 +71,66 @@
 
     </main>
 
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/interactjs/dist/interact.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.arrow-icon').click(function() {
+                toggleComments($(this));
+            });
+        });
+
+        function toggleComments(arrowButton) {
+            const expandComments = arrowButton;
+            const comments = $(arrowButton).parent().next();
+            const isCommentsVisible = comments.hasClass('active');
+
+
+            if (!isCommentsVisible) {
+                comments.addClass('active').css('display', 'flex').hide().slideDown(200);
+                expandComments.css('transform', 'rotate(180deg)');
+
+            } else {
+                comments.removeClass('active').slideUp(200);
+                expandComments.css('transform', 'rotate(0deg)');
+
+            }
+        };
+
+
+        const position = { x: 0, y: 0 }
+
+        interact('.draggable').draggable({
+            modifiers: [
+                    interact.modifiers.restrictRect({
+                    restriction: 'parent'
+                })
+                ],
+            listeners: {
+
+                move (event) {
+                position.x += event.dx
+                position.y += event.dy
+
+                event.target.style.transform =
+                    `translate(${position.x}px, ${position.y}px)`
+                },
+            }
+        })
+
+    </script>
+
+    <style>
+
+        .dragging-container,
+        .dragging-container * {
+            -ms-touch-action: none;
+            touch-action: none;
+        }
+
+    </style>
+
 @endsection
+
