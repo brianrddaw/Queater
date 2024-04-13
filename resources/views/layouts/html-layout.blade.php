081e46dec6c4c8e0bb916a14f3f04e0aa3f49f4c
brianrddaw <brianramirezdaw@gmail.com>
2024-04-13 11:59:00 +0200

    mejorado diseño de user.main

--- resources/views/layouts/html-layout.blade.php
+++ resources/views/layouts/html-layout.blade.php
@@ -12,8 +12,8 @@
     <title>@yield('title')</title>
 </head>
 <body class="overflow-x-hidden bg-walter-200">
-    <header class="flex justify-items-center items-center  bg-orange-500 p-2 h-14 text-zinc-100 ">
-        <img src="{{ asset('imgs/letras.webp') }}" alt="" class="pl-2 h-4 mr-auto  ">
+    <header class="flex justify-items-center items-center  bg-orange-500 p-2 h-14 text-orange-50 ">
+        <img src="{{ asset('imgs/letras.webp') }}" alt="" class="pl-2 h-4 mr-auto">
 
         @if (!empty(trim($__env->yieldContent('navegacion'))))
         <nav class="flex flex-row items-center ml-auto mr-2 ">
