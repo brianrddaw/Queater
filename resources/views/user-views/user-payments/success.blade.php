347b835e8c0d6eb1fefbdda8ca3f157c80179406
brianrddaw <brianramirezdaw@gmail.com>
2024-04-15 21:06:36 +0200

    cambios menores

--- resources/views/user-views/user-payments/success.blade.php
+++ resources/views/user-views/user-payments/success.blade.php
@@ -6,9 +6,8 @@
         <img class="w-64" src="{{ asset('imgs/chef.webp') }}" alt="">
         <h1 class="font-bold text-lg">Tu pedido se ha mandado a cocina</h1>
         <span class="flex flex-col w-fit h-full items-center gap-4 mt-6">
-            <a class="min-w-fit w-44  text-center h-12 flex items-center justify-center  bg-orange-500 rounded font-bold text-orange-50" href="/printTicket/{{ $orderId }}">Imprimir ticket</a>
-            <a class="min-w-fit w-44  text-center h-12 flex items-center justify-center  bg-orange-500 rounded font-bold text-orange-50" href="/">Volver al menu</a>
+            <a class="min-w-fit w-44 h-14 text-center  flex items-center justify-center  bg-orange-500 rounded font-bold text-orange-50" href="/printTicket/{{ $orderId }}">Imprimir ticket</a>
+            <a class="min-w-fit w-44 h-14 text-center  flex items-center justify-center  bg-orange-500 rounded font-bold text-orange-50" href="/">Volver al menu</a>
         </span>
-
     </section>
 @endsection
