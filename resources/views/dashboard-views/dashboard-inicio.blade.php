5c0178e3a923688a2d3da42be5737a51d64300db
brianrddaw <brianramirezdaw@gmail.com>
2024-04-19 00:02:10 +0200

    cambios menores en dashboard inicio

--- resources/views/dashboard-views/dashboard-inicio.blade.php
+++ resources/views/dashboard-views/dashboard-inicio.blade.php
@@ -2,111 +2,72 @@
 <h1 class="flex items-center justify-center font-bold text-orange-950 text-xl bg-walter-400  h-[3.75rem]">
     Bienvenido al menú de inicio
 </h1>
-<section class="flex flex-col gap-10 w-full h-fit p-2">
-    <div class="chart-container h-fit">
+<section class="flex flex-col gap-10 w-full h-full p-2">
 
-        <div class="flex justify-center items-center h-fit w-full p-2">
-            <canvas id="myChart"></canvas>
-
-        </div>
-        {{-- <table class="charts-css bar h-fit">
-            <tr class="border border-green-500">
-                <td style="--start: 0; --end: 0.3; --color: #f97316">
-                    <span class="data border border-blue-500"> $ 40K </span>
-                </td>
-            </tr>
-            <tr>
-                <td style="--start: 0.3; --end: 1;">
-                    <span class="data "> $ 30K </span>
-                </td>
-            </tr>
-        </table> --}}
-
+    <div class="flex justify-center items-center h-fit w-full max-w-[900px] p-2 border border-red-500 mx-auto">
+        <canvas id="lineChart"></canvas>
     </div>
 
     <div class="flex w-full h-fit gap-4">
 
-            <a href="{{route('dashboard.products')}}" class="flex items-center justify-center w-full h-20  font-bold text-orange-950 bg-orange-50 drop-shadow-xl rounded">
-                <p>Productos</p>
-            </a>
-            <a href="{{route('dashboard.categories')}}" class="flex items-center justify-center w-full h-20  font-bold text-orange-950 bg-orange-50 drop-shadow-xl rounded">
-                <p>Categorias</p>
-            </a>
-            <a href="{{route('dashboard.tables')}}" class="flex items-center justify-center w-full h-20  font-bold text-orange-950 bg-orange-50 drop-shadow-xl rounded">
-                <p>Mesas</p>
-            </a>
+        <a href="{{route('dashboard.products')}}" class="flex items-center justify-center w-full h-20  font-bold text-orange-950 bg-orange-50 drop-shadow-xl rounded">
+            <p>Productos</p>
+        </a>
+        <a href="{{route('dashboard.categories')}}" class="flex items-center justify-center w-full h-20  font-bold text-orange-950 bg-orange-50 drop-shadow-xl rounded">
+            <p>Categorias</p>
+        </a>
+        <a href="{{route('dashboard.tables')}}" class="flex items-center justify-center w-full h-20  font-bold text-orange-950 bg-orange-50 drop-shadow-xl rounded">
+            <p>Mesas</p>
+        </a>
 
     </div>
 
 </section>
 
-<style>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
 
-    label{
-
+    function getLastSevenDays()
+    {
+        const date = new Date();
+        const days = [];
+        for (let i = 0; i < 7; i++) {
+            const day = date.getDate() - i;
+            days.push(day);
+        }
+        return days;
     }
 
-</style>
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-<script>
-    const data = {
-        labels: [
-            'Hamburguesas ',
-            'Ensaladas',
-            'Batidos',
-            'Perkins',
-            'Pepes',
-        ],
-
-        datasets: [{
-            data: [300, 100, 50, 34, 15],
-            backgroundColor: [
-                '#f97316',
-                '#ffa04d',
-                '#ffc686',
-                '#ffdabf',
-                '#ffedd9',
-            ],
-            hoverOffset: 4,
-            spacing: 8,
-            borderRadius: 3
-        }]
-    };
-
-    const plugin = {
-        id: 'customCanvasBackgroundColor',
-        beforeDraw: (chart, args, options) => {
-            const {ctx} = chart;
-            ctx.save();
-            ctx.globalCompositeOperation = 'destination-over';
-            ctx.fillStyle = options.color || '#19ffff';
-            ctx.fillRect(0, 0, chart.width, chart.height);
-            ctx.restore();
-        }
+    const data =
+    {
+        labels: getLastSevenDays(),
+        datasets: [
+            {
+                data: [300, 100, 50, 34, 15, 20, 30],
+                tension: 0,
+                fill: false,
+                borderColor: '#f97316',
+            }
+        ]
     };
 
     const config = {
-        type: 'doughnut',
+        type: 'line',
         data: data,
         options: {
             plugins: {
                 legend: {
-
-                    align: 'start' // Establecer la alineación de los labels a la izquierda
-
+                    align: 'center'
                 },
                 customCanvasBackgroundColor: {
                     color: 'transparent',
                 }
             }
+
         },
-        plugins: [plugin],
     };
 
-    // Renderizar el gráfico
-    const ctx = document.getElementById('myChart').getContext('2d');
+    const ctx = document.getElementById('lineChart').getContext('2d');
     new Chart(ctx, config);
 </script>
 
